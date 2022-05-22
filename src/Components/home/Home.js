179cbe14d5a44902e92cce99bf3044a7b2ff921c
hector <martzluna.hm@gmail.com>
2022-05-22 15:59:06 -0500

    cards

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import SectionOne from './sectionOne';
+import SectionTwoHome from './SectionTwoHome';
 
 export default function Home() {
   return (
     <div>
       <SectionOne />
-      
+      <SectionTwoHome/>
     </div>
   )
 }
---
+++ src/Components/home/SectionTwoHome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FeaturedCards from '../FeaturedCard'
+
+function SectionTwoHome() {
+  return (
+    <FeaturedCards/>
+  )
+}
+
+export default SectionTwoHome
