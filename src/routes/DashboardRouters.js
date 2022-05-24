cb9316930dacda502b49dcf1cc536fcf4ab41172
unknown <jarlingduqueduarte@gmail.com>
2022-05-23 19:34:24 -0500

    form-Agente

--- src/routes/DashboardRouters.js
+++ src/routes/DashboardRouters.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../Components/home/Home'
-
 import NavBars from '../Components/NavBars'
+import Agentes from '../Components/Agentes'
 
 
 
@@ -16,6 +16,8 @@ const DashboardRouters = () => {
         <Route path="/" element={<Home />}/>
           
           <Route path="*" element={<Navigate to="/" />}/>
+
+          <Route path="/agente" element={<Agentes />}/>
         </Routes>
       </>
     </div>
